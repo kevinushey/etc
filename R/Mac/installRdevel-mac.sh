be4aef4dc6755f6194408e00fe285031db5a277a
Kevin Ushey <kevinushey@gmail.com>
2015-04-05 01:05:28 -0700

    update

--- R/Mac/installRdevel-mac.sh
+++ R/Mac/installRdevel-mac.sh
@@ -11,9 +11,30 @@
 ## the lapack / openblas links are broken. You can either fix this
 ## manually with otool, or be lazy and just reinstall openblas and
 ## lapack (in that order).
+##
 
-OWD="$(pwd)"
-CLANG=clang
+## ----- CONFIGURATION VARIABLES ----- ##
+
+# Installation-related
+: ${INSTALLDIR:=/Library/Frameworks}        # NOTE: needs 'sudo' on make install
+: ${SOURCEDIR:=~/R-devel}                   # checked out R sources will live here
+: ${TMP:=${HOME}/tmp}                       # temporary dir used on installation
+
+## Compiler-specific
+: ${CC:=clang}
+: ${CXX:=clang++}
+: ${CFLAGS:=-g -O3 -Wall -pedantic}
+: ${CXXFLAGS:=-g -O3 -Wall -pedantic}
+: ${FORTRAN:=gfortran}
+: ${FFLAGS:=-g -O3} 
+: ${OBJC:=clang}
+: ${OBJCFLAGS:=${CFLAGS}}
+: ${MAKE:=make}
+: ${MAKEFLAGS:=-j10}
+
+## ----- END CONFIGURATION VARIABLES ----- ##
+
+OWD=`pwd`
 
 ## check if homebrew is installed
 echo "Checking for Homebrew..."
@@ -24,7 +45,7 @@ else
     ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go/install)"
 fi
 
-export TMP=~/.tmp
+export TMP=${TMP}
 mkdir ${TMP} 2> /dev/null
 
 missing () {
@@ -36,47 +57,51 @@ missing () {
 install () {
     if test -z `missing $1`; then
         brew install $1
-        brew link $1
+        brew link --force $1
     fi
 }
 
 ## make sure we have the necessary taps
 brew tap homebrew/science 2> /dev/null
-brew tap homebrew/dupes 2> /dev/null
+brew tap homebrew/dupes   2> /dev/null
 
 ## use homebrew to install openblas, lapack, gfortran
-install openblas
-install lapack
-install texinfo
-install coreutils # greadlink
 
-## Make sure the gfortran libraries get symlinked
-if command -v gfortran 2> /dev/null; then
+install gcc                        # gfortran
+install openblas                   # faster BLAS
+install lapack                     # latest lapack
+install texinfo                    # vignettes, help
+install coreutils                  # greadlink
+install jpeg                       # image write support
+install cairo                      # image write support
+
+## Make sure the gfortran libraries get symlinked.
+if command -v gfortran &> /dev/null; then
 	GFORTRAN=gfortran
-elif command -v gfortran-4.9 2> /dev/null; then
+elif command -v gfortran-4.9 &> /dev/null; then
 	GFORTRAN=gfortran-4.9
 fi
 
+## Do some path munging and symlink fortran libraries
+## to /usr/local/lib
 GFORTRAN_BINPATH=`which ${GFORTRAN} | xargs greadlink -f | xargs dirname`
 GFORTRAN_LIBPATH=${GFORTRAN_BINPATH}/../lib/gcc/4.9/
 GFORTRAN_LIBPATH=`greadlink -f ${GFORTRAN_LIBPATH}`
 
-for file in ${GFORTRAN_LIBPATH}/libgfortran*; do
-    echo Symlinking file: ${file##*/}
-    rm /usr/local/lib/${file##*/} 2> /dev/null
-    ln -fs "${file}" /usr/local/lib/${file##*/}
+for FILEPATH in ${GFORTRAN_LIBPATH}/libgfortran*; do
+	BASENAME=${FILEPATH##*/}
+	LINKEDPATH=/usr/local/lib/${BASENAME}
+	if test -e "${LINKEDPATH}"; then
+		echo File \'${LINKEDPATH}\' already exists, not symlinking
+	else
+		echo Symlinking \'${BASENAME}\' to \'${LINKEDPATH}\'
+	fi
 done;
-
-## some other things we need
-install jpeg
-
-## We need to link in Cairo
-install cairo
 
 ## Download R-devel from SVN
 cd ~
-mkdir R-devel 2> /dev/null
-cd R-devel
+mkdir -p ${SOURCEDIR} &> /dev/null
+cd ${SOURCEDIR}
 echo "Checking out latest R..."
 svn checkout https://svn.r-project.org/R/trunk/
 cd trunk
@@ -86,47 +111,31 @@ cd trunk
 echo "Syncing recommended R packages..."
 ./tools/rsync-recommended
 
+## This was needed for building some applications that included
+## 'Rinterface.h' in multiple translation units (we were encountering
+## linker errors)
 echo Adding a missing extern in 'Rinterface.h'...
-sed -i '' 's/^int R_running_as_main_program/extern int R_running_as_main_program/g' include/Rinterface.h
 sed -i '' 's/^int R_running_as_main_program/extern int R_running_as_main_program/g' src/include/Rinterface.h
-
 
 ## For some reason, there is trouble in locating Homebrew Cairo;
 ## we have to make sure pkg-config looks in the right place
 export PKG_CONFIG_PATH=/opt/X11/lib/pkgconfig
 
-## Use a faster version of R_IsNA -- ie, we just check if the
-## value of the double is one of the two 'possible' values for NA
-echo Using a faster version of R_IsNA, R_IsNaN...
-cd ..
-if test ! -f "new_NA_behavior.diff"; then
-    wget https://raw.githubusercontent.com/kevinushey/etc/master/R/new_NA_behavior.diff
-fi;
-cd trunk
-
-## apply the patch -- okay if this fails, but we should figure out
-## how to do so more gracefully
-patch -p0 -f -s -i ../new_NA_behavior.diff > /dev/null
-if test -e "src/main/arithmetic.c.rej"; then
-    rm "src/main/arithmetic.c.rej"
-fi;
-echo Patch applied.
-
 ## modify config.site so that it gets the appropriate compilers etc
-rm config.site
-touch config.site
-echo CC=\"${CLANG}\" >> config.site
-echo CFLAGS=\"-g -O0\" >> config.site
-echo CXX=\"${CLANG}++\" >> config.site
-echo CXXFLAGS=\"-g -O0\" >> config.site
-echo F77=\"gfortran\" >> config.site
-echo FFLAGS=\"-g -O0\" >> config.site
-echo FC=\"gfortran\" >> config.site
-echo FCFLAGS=\"-g -O0\" >> config.site
-echo OBJC=\"${CLANG}\" >> config.site
-echo OBJCFLAGS=\"-g -O0\" >> config.site
-echo MAKE=\"make\" >> config.site
-echo MAKEFLAGS=\"-j10\" >> config.site
+cat > config.site <<- EOM
+CC="${CC}"
+CFLAGS="${CFLAGS}"
+CXX="${CXX}"
+CXXFLAGS="${CXXFLAGS}"
+F77="${FORTRAN}"
+FFLAGS="${FFLAGS}"
+FC="${FORTRAN}"
+FCFLAGS="${FFLAGS}"
+OBJC="${OBJC}"
+OBJCFLAGS="${OBJCFLAGS}"
+MAKE="${MAKE}"
+MAKEFLAGS="${MAKEFLAGS}"
+EOM
 
 make distclean
 make clean
@@ -142,12 +151,18 @@ make clean
     --enable-R-profiling \
     --enable-memory-profiling \
     --with-valgrind-instrumentation=2 \
-    --without-internal-tzcode
+    --without-internal-tzcode \
+    --prefix=${INSTALLDIR} \
+    $@
 
 make -j10
 
-echo "Installing to system library: please enter your password so we can 'sudo make install'"
-sudo make install
+if test "${INSTALLDIR}" = "/Library/Frameworks"; then
+	echo Installing to system library: please enter your password
+	sudo make install
+else
+	make install
+fi
 
 echo Installation completed successfully\!
 cd ${OWD}
