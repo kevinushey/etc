84ca6a9f5207b27a914a59f0ea23013cb3128f83
Kevin Ushey <kevinushey@gmail.com>
2015-04-28 18:18:23 -0700

    set up git

--- apply-windows.cmd
+++ apply-windows.cmd
@@ -12,5 +12,15 @@ DEL "%EMACS_DOTFILE_TO%"
 
 REM Would prefer symlink here but that requires admin privileges.
 MKLINK /H %EMACS_DOTFILE_TO% %EMACS_DOTFILE_FROM%
+
+REM Set up Git.
+SET "OLDPATH=%PATH%"
+SET "PATH=C:\Program Files (x86)\Git\bin;%PATH%"
+git config --global user.name "Kevin Ushey"
+git config --global user.email "kevinushey@gmail.com"
+git config --global push.default simple
+git config --global credential.helper 'cache --timeout=3600'
+SET "PATH=%OLDPATH%"
+
 PAUSE
 
