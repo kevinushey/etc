ba04cccd75712987d1cfbc39e73c5dc216c078d3
Kevin Ushey <kevinushey@gmail.com>
2015-05-26 17:43:50 -0700

    update apply

--- apply.sh
+++ apply.sh
@@ -1,35 +1,54 @@
 #!/usr/bin/env bash
 PWD=`pwd`
 
-if [ $(uname) = "Linux" ]; then
+## Some initial setup to figure out who we are
+UNAME=`uname`
+if [ "${UNAME}" = "Linux" ]; then
+	echo "System: Linux"
+	IS_LINUX=yes
+elif [ "${UNAME}" = "Darwin" ]; then
+	echo "System: Darwin"
+	IS_DARWIN=yes
+fi
+
+if [ -n "$(lsb_release --id | grep Ubuntu)" ]; then
+	echo "Linux Type: Ubuntu"
+	IS_UBUNTU=yes
+fi
+
+if [ -n "${IS_UBUNTU}" ]; then
 	./apply-ubuntu.sh
 fi
 
-## Bash
-if [ $(uname) = "Linux" ]; then
-    BASHRC="${HOME}/.bashrc"
+## Set the 'bash' dotfile path
+if [ -n "${IS_LINUX}" ]; then
+    BASH_DOTFILE_PATH="${HOME}/.bashrc"
 else
-    BASHRC="${HOME}/.bash_profile"
+    BASH_DOTFILE_PATH="${HOME}/.bash_profile"
 fi
 
-ln -fs ${PWD}/Bash/.bash_profile "${BASHRC}"
-source ~/.bash_profile
+ln -fs ${PWD}/Bash/.bash_profile "${BASH_DOTFILE_PATH}"
 
 ## R
 ln -fs ${PWD}/R/.Rprofile ~/.Rprofile
 
 ## go
-echo "Installing Go utilities..."
-go get -u github.com/nsf/gocode
-go get -u code.google.com/p/go.tools/cmd/goimports
-go get -u code.google.com/p/rog-go/exp/cmd/godef
-go get -u code.google.com/p/go.tools/cmd/godoc
-go get -u code.google.com/p/go.tools/cmd/vet
+# echo "Installing Go utilities..."
+# go get -u github.com/nsf/gocode
+# go get -u code.google.com/p/go.tools/cmd/goimports
+# go get -u code.google.com/p/rog-go/exp/cmd/godef
+# go get -u code.google.com/p/go.tools/cmd/godoc
+# go get -u code.google.com/p/go.tools/cmd/vet
 
 ## Emacs
 ln -fs ${PWD}/Emacs/.emacs ~/.emacs
+
+mkdir -p ~/.emacs.d
 ln -fs ${PWD}/Emacs/snippets ~/.emacs.d/snippets
-cp "${PWD}/Mac/Emacs/Emacs Daemon.app" "/Applications/Emacs Daemon.app"
+
+if [ -n "${IS_DARWIN}" ]; then
+	cp "${PWD}/Mac/Emacs/Emacs Daemon.app" "/Applications/Emacs Daemon.app"
+fi
 
 ## Vim
 if [ ! -e "${HOME}/.spf13-vim-3" ]; then
@@ -66,3 +85,4 @@ mkdir -p ~/.config/QtProject/qtcreator/snippets
 cp ${PWD}/QtCreator/styles/*.xml ~/.config/QtProject/qtcreator/styles/
 cp ${PWD}/QtCreator/snippets/snippets.xml ~/.config/QtProject/qtcreator/snippets/snippets.xml
 cp ${PWD}/QtCreator/schemes/* ~/.config/QtProject/qtcreator/schemes/
+
