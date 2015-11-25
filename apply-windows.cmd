da3161a29ab66dda4bff5b68be4bcde1686b6f71
Kevin Ushey <kevinushey@gmail.com>
2015-11-24 23:36:59 -0800

    update windows apply

--- apply-windows.cmd
+++ apply-windows.cmd
@@ -5,13 +5,13 @@ set "PWD=%cd%"
 set "HOME=%USERPROFILE%"
 
 REM Make a symlink for .emacs
-set "EMACS_DOTFILE_FROM=%PWD%\Emacs\.spacemacs"
+set "EMACS_DOTFILE_FROM=%PWD%\editor\emacs\.spacemacs"
 set "EMACS_DOTFILE_TO=%HOME%\AppData\Roaming\.spacemacs"
 
 if exist "%EMACS_DOTFILE_TO%" del "%EMACS_DOTFILE_TO%"
 
 REM Would prefer symlink here but that requires admin privileges,
-REM something not supplied on non-professional Windows installs. :/
+REM something not supplied on non-professional Windows installs.
 mklink /H "%EMACS_DOTFILE_TO%" "%EMACS_DOTFILE_FROM%"
 
 REM Set up Git.
@@ -24,7 +24,7 @@ git config --global credential.helper wincred
 
 REM Set up _vimrc.
 if not exist "%HOME%\.vim" mkdir "%HOME%\.vim"
-set "VIMRC_DOTFILE_FROM=%PWD%\Vim\.vimrc"
+set "VIMRC_DOTFILE_FROM=%PWD%\editor\vim\.vimrc"
 set "VIMRC_DOTFILE_TO=%HOME%\_vimrc"
 
 rm "%VIMRC_DOTFILE_TO%"
@@ -32,7 +32,7 @@ mklink /H "%VIMRC_DOTFILE_TO%" "%VIMRC_DOTFILE_FROM%"
 mklink /H "%HOME%\.vimrc" "%VIMRC_DOTFILE_TO%"
 
 REM Link in other supporting Vim files.
-set "VIM_STARTUP_DIR=%PWD%\Vim\startup"
+set "VIM_STARTUP_DIR=%PWD%\editor\vim\startup"
 mklink /J "%HOME%\.vim\startup" "%VIM_STARTUP_DIR%"
 
 REM Copy over Qt Creator related items.
@@ -42,12 +42,12 @@ mkdir "%QT_CREATOR_DIR%\styles"
 mkdir "%QT_CREATOR_DIR%\schemes"
 mkdir "%QT_CREATOR_DIR%\snippets"
 
-copy "%PWD%\QtCreator\schemes\keyboard.kms" "%QT_CREATOR_DIR%\schemes\keyboard.kms"
-copy "%PWD%\QtCreator\snippets\snippets.xml" "%QT_CREATOR_DIR%\snippets\snippets.xml"
-copy "%PWD%\QtCreator\styles\Tomorrow-Night-Bright.xml" "%QT_CREATOR_DIR%\styles\Tomorrow-Night-Bright.xml"
+copy "%PWD%\editor\qt\schemes\keyboard.kms" "%QT_CREATOR_DIR%\schemes\keyboard.kms"
+copy "%PWD%\editor\qt\snippets\snippets.xml" "%QT_CREATOR_DIR%\snippets\snippets.xml"
+copy "%PWD%\editor\qt\styles\Tomorrow-Night-Bright.xml" "%QT_CREATOR_DIR%\styles\Tomorrow-Night-Bright.xml"
 
 REM AutoHotkey.
-copy "%PWD%\Windows\AutoHotkey.ahk" "%USERPROFILE%\Documents\AutoHotkey.ahk"
+copy "%PWD%\platform\windows\AutoHotkey.ahk" "%USERPROFILE%\Documents\AutoHotkey.ahk"
 
 PAUSE
 
