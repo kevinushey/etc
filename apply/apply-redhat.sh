2604eb554a42243126648f95a0390c000d3fc6ca
Kevin Ushey <kevinushey@gmail.com>
2017-06-15 20:04:29 -0700

    tweak

--- apply/apply-redhat.sh
+++ apply/apply-redhat.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 
-if [ -n "$(grep "CentOS Linux release 7" /etc/redhat-release 2> /dev/null)" ]; then
-	wget -c http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
-	sudo rpm -ivh epel-release-7-5.noarch.rpm
-else
-	echo "Failed to detect CentOS version: not activating EPEL"
-fi
-
+sudo yum install -y epel-release
 sudo yum update -y
 sudo yum install -y      \
 	R                    \
