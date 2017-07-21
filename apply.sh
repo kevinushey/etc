d5036a202f2999587780aca10a0d5fbff9cf9c8c
Kevin Ushey <kevinushey@gmail.com>
2017-07-21 15:01:19 -0700

    tweaks for symlinking + docker

--- apply.sh
+++ apply.sh
@@ -4,6 +4,8 @@
 ROOT="$(cd "$(dirname "$0")"; pwd -P)"
 pushd "${ROOT}" &> /dev/null
 
+RELROOT="${ROOT/$HOME\//}"
+
 # bootstrapping of bash support scripts
 for FILE in login/bash/bash*; do . "${FILE}"; done
 
@@ -11,10 +13,12 @@ is-redhat && import apply/apply-redhat.sh
 is-ubuntu && import apply/apply-ubuntu.sh
 
 ## Symlink all dotfiles
-ln -nfs "${ROOT}"/dotfiles/.??* ~/
-ln -nfs "${ROOT}"/tmux ~/.tmux
-ln -nfs "${ROOT}"/login ~/.login
-ln -nfs ~/.bash_profile ~/.bashrc
+pushd "${HOME}"
+ln -nfs "${RELROOT}"/dotfiles/.??* .
+ln -nfs "${RELROOT}"/tmux          .tmux
+ln -nfs "${RELROOT}"/login         .login
+ln -nfs .bash_profile              .bashrc
+popd
 
 ## tmux
 mkdir -p ~/projects
@@ -34,18 +38,27 @@ git fetch && git reset --hard origin/master
 
 rm -rf ~/.emacs.d/snippets
 rm -rf ~/.emacs.d/private
-ln -nfs "${ROOT}"/editor/emacs/snippets ~/.emacs.d/snippets
-ln -nfs "${ROOT}"/editor/emacs/private-layers ~/.emacs.d/private
-ln -nfs "${ROOT}"/editor/emacs/user-config.el ~/.emacs.d/user-config.el
-ln -nfs "${ROOT}"/editor/emacs/user-init.el ~/.emacs.d/user-init.el
+
+pushd "${HOME}"
+ln -nfs "../${RELROOT}"/editor/emacs/snippets       .emacs.d/snippets
+ln -nfs "../${RELROOT}"/editor/emacs/private-layers .emacs.d/private
+ln -nfs "../${RELROOT}"/editor/emacs/user-config.el .emacs.d/user-config.el
+ln -nfs "../${RELROOT}"/editor/emacs/user-init.el   .emacs.d/user-init.el
+popd
 
 ## Vim
-mkdir -p ~/.vim
-rm -rf ~/.vim/startup ~/.vim/after
-ln -nfs ~/.vimrc ~/.nvimrc
-for FILE in "${ROOT}"/editor/vim/*; do
-	ln -nfs "${FILE}" ~/.vim/
+pushd "${HOME}"
+mkdir -p .vim
+rm -rf .vim/startup .vim/after
+ln -nfs .vimrc .nvimrc
+
+pushd .vim
+for FILE in ../"${RELROOT}"/editor/vim/*; do
+	ln -nfs "${FILE}" "$(basename "${FILE}")"
 done
+popd
+
+popd
 
 ## Helper scripts to be put on the PATH (Darwin only)
 if is-darwin; then
@@ -66,9 +79,11 @@ mkdir -p ~/.config/QtProject/qtcreator/styles
 mkdir -p ~/.config/QtProject/qtcreator/schemes
 mkdir -p ~/.config/QtProject/qtcreator/snippets
 
-ln -nfs "${ROOT}"/editor/qt/styles/*.xml ~/.config/QtProject/qtcreator/styles/
-ln -nfs "${ROOT}"/editor/qt/snippets/snippets.xml ~/.config/QtProject/qtcreator/snippets/snippets.xml
-ln -nfs "${ROOT}"/editor/qt/schemes/* ~/.config/QtProject/qtcreator/schemes/
+pushd "${HOME}"
+ln -nfs "${RELROOT}"/editor/qt/styles/*.xml          .config/QtProject/qtcreator/styles/
+ln -nfs "${RELROOT}"/editor/qt/snippets/snippets.xml .config/QtProject/qtcreator/snippets/snippets.xml
+ln -nfs "${RELROOT}"/editor/qt/schemes/*             .config/QtProject/qtcreator/schemes/
+popd
 
 popd &> /dev/null
 
