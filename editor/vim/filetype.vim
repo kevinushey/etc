5c1c8ab48c3ac9ad5d2aed3e99722ccd1e258503
Kevin Ushey <kevinushey@gmail.com>
2018-10-24 21:44:25 -0700

    fzf commands; C-X C-E filetype

--- editor/vim/filetype.vim
+++ editor/vim/filetype.vim
@@ -1,14 +1,15 @@
 if exists('did_load_filetypes')
-	finish
+    finish
 endif
 
 augroup filetypedetect
-	autocmd!
+    autocmd!
 
-	autocmd BufRead,BufNewFile .Rprofile  set filetype=r
+    autocmd BufRead,BufNewFile .Rprofile  set filetype=r
     autocmd BufRead,BufNewFile .eslintrc  set filetype=json
     autocmd BufRead,BufNewFile .Renviron  set filetype=sh
     autocmd BufRead,BufNewFile CMake*.txt set filetype=cmake
+    autocmd BufRead,BufNewFile bash-fc*   set filetype=sh
 
 augroup END
 
--- login/bash/fzf.sh
+++ login/bash/fzf.sh
@@ -4,7 +4,7 @@
 if has-command fzf; then
 
     if has-command rg; then
-        FZF_DEFAULT_COMMAND='rg --files --hidden --follow --no-ignore-parent --glob "!.git/*"'
+        FZF_DEFAULT_COMMAND='rg --files --hidden --no-follow --no-ignore-parent --glob "!.git/*"'
     elif has-command ag; then
         FZF_DEFAULT_COMMAND='ag -g ""'
     fi
@@ -13,11 +13,20 @@ if has-command fzf; then
         export FZF_DEFAULT_COMMAND
     fi
 
+
+    FZF_CTRL_T_COMMAND="${FZF_DEFAULT_COMMAND}"
+    export FZF_CTRL_T_COMMAND
+
     FZF_CTRL_T_OPTS="--preview '(highlight -O ansi -l {} 2> /dev/null || cat {} || tree -C {}) 2> /dev/null | head -200'"
     export FZF_CTRL_T_OPTS
 
+
+    FZF_ALT_C_COMMAND="${FZF_DEFAULT_COMMAND}"
+    export FZF_ALT_C_COMMAND
+
     FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
     export FZF_ALT_C_OPTS
+
 
     # Use Alt-; to trigger fzf's autocompletion behavior
     bind '"\e;": "**	"'
