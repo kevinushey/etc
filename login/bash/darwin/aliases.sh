41157e6498500bf946427695943f3567e39a4510
Kevin Ushey <kevinushey@gmail.com>
2025-05-22 14:44:20 -0700

    vvv

--- login/bash/darwin/aliases.sh
+++ login/bash/darwin/aliases.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-alias ll='ls -laGFh'
+alias ll='ls -laGFhv'
 alias lsregister=/System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister
 
--- login/bash/linux/aliases.sh
+++ login/bash/linux/aliases.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 alias ls='ls --color'
-alias ll='ls -laGFh --color'
+alias ll='ls -laGFhv --color'
 alias qtcreator="qtcreator -noload Welcome -noload QmlDesigner -noload QmlProfiler"
 
